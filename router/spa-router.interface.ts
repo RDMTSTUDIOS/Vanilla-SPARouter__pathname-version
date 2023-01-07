58c4e4f5dd019f44ba580b266a51f7f278cdabb9
VNDG <rdmtstudios@yandex.ru>
2023-01-07 21:27:51 +0300

    Delete router directory

--- router/spa-router.interface.ts
+++
@@ -1,8 +0,0 @@
-import {AppInterface} from "../app/app.interface";
-import {PageProtoInterface} from "../../pages/page.interface";
-
-export interface SPA_RouterInterface
-{
-    ConfigurePath: (pathName: string, page: PageProtoInterface) => void;
-    StartRouting(SPA_APP: AppInterface): void;
-}
